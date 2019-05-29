060ccba34cac1c6a1134d2105b3ce7ef931c5c59
Heejung Chung <hchung98@sh-ln04.stanford.edu>
2019-05-29 02:33:32 -0700

    added normalized decision boundary histogram, as well as for svm trained on fake rsfs

--- rsf_svm.py
+++ rsf_svm.py
@@ -81,29 +81,40 @@ def validation_curve(X, y, fname="linearsvm_validation.png"):
     plot_scores(train_scores, test_scores, "Validation Curve with SVM", param_range, 
                 fname, plt.semilogx, "C")
 
-def plot_SVM_confidence(X, y):
+def plot_SVM_confidence(X, y, sep_val_confidence=False, X_val=None, y_val=None, fname="confidence.png", title="Decision Function"):
+    if not sep_val_confidence:
+        X_val = X
+        y_val = y
     clf = svm.LinearSVC(C=10)
     clf.fit(X,y)
-    confidence = clf.decision_function(X)
+    confidence = clf.decision_function(X_val)
     maxconf = confidence.max()
     minconf = confidence.min()
+    
     bins = np.linspace(minconf, maxconf, 100)
-    plt.hist(confidence[y==1], bins=bins, alpha=.5, label="liq", edgecolor="k")
-    plt.hist(confidence[y==0], bins=bins, alpha=.6, label="ice", edgecolor="k")
-    plt.title("Decision function for Linear SVM training data")
+    plt.hist(confidence[y_val==1], bins=bins, alpha=.5, label="liq", edgecolor="k")
+    plt.hist(confidence[y_val==0], bins=bins, alpha=.6, label="ice", edgecolor="k")
+    plt.title(title)
     plt.legend(loc="best")
-    plt.savefig("confidence.png")
+    plt.savefig(fname)
     plt.clf()
 
 X_train, y_train, X_val, y_val, hdrs = compile_rsfs_data()
 #pdb.set_trace()
-#pseudo_X = rsf_load_data.pseudo_rsfs_data(X_train, y_train, hdrs)
-#scaler, X_train = rsf_load_data.scale_data(X_train)
-#pseudo_scaler, pseudo_X = rsf_load_data.scale_data(pseudo_X)
+pseudo_X = rsf_load_data.pseudo_rsfs_data(X_train, y_train, hdrs)
+scaler, X_train = rsf_load_data.scale_data(X_train)
+pseudo_scaler, pseudo_X = rsf_load_data.scale_data(pseudo_X)
 #validation_curve(pseudo_X, y_train, "pseudorsfs_linearsvm_validation.png")
 #learning_curve(pseudo_X, y_train, fname="pseudorsfs_linearsvm_learning.png")
 
-plot_SVM_confidence(X_train, y_train)
+plot_SVM_confidence(pseudo_X, y_train, 
+                    sep_val_confidence=True,
+                    X_val=pseudo_scaler.transform(X_val), y_val=y_val, 
+                    fname="fake_confidence.png", 
+                    title="Decision function for Linear SVM trained on fake RSFs")
+plot_SVM_confidence(X_train, y_train,
+                    fname="confidence.png", 
+                    title="Decision function for Linear SVM training data")
 #simple_SVM(X_train, y_train, X_val, y_val)
 #simple_SVM(pseudo_X, y_train, pseudo_scaler.transform(X_train), y_train)
 #simple_SVM(X_train, y_train, scaler.transform(X_val), y_val)
